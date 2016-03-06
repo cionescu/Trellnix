d5c6a47d47a0a34774f6f023ca35935491303080
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-03-06 12:03:12 -0800

    [config] Update dev to work with local postgres

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :trellnix, Trellnix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "",
+  password: "",
   database: "trellnix_dev",
   hostname: "localhost",
   pool_size: 10
